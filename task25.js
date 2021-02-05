7bf42f7fde7658b9698e14869bb951ffce2d7a4e
Eric Belecov <ericbelecov@gmail.com>
2021-02-05 07:38:25 +0300

    Second commit

---
+++ task25.js
@@ -0,0 +1,14 @@
+// Задача №25
+
+// Напишите функцию capitalizeFirstLetter(str), которая преобразоввывает 
+// первый символ строки в нижний регистр.
+// let str="Пример строки";
+// function capitalizeFirstLetter(str) {};
+// capitalizeFirstLetter(str)
+
+function capitalizeFirstLetter(str) {
+    return str[0].toLowerCase() + str.slice(1); // извлекаем первый символ строки, преобразовываем его в нижнию строку 
+    // и конкатинируем с остальной частью строки
+};
+console.log(capitalizeFirstLetter('Test')); // --> 'test'
+console.log(capitalizeFirstLetter('Пример')); // --> 'пример'
