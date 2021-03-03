373ab8ca952465d9ef93395b5a58d415a9438fb1
Eric Belecov <ericbelecov@gmail.com>
2021-03-04 02:38:10 +0300

    Second commit

--- task11.js
+++ task11.js
@@ -8,18 +8,13 @@
 
 // Ты можете предположить, что все значения являются целыми числами. Не изменяй входной массив / список
 
-let numForInvert0 = ([-1, -2, -4, -5]);
-let numForInvert1 = ([1, 2, 4, 5]);
-let numForInvert2 = ([1,-2,4,-5]);
+let numForInvert0 = [-1, -2, -4, -5];
+let numForInvert1 = [1, 2, 4, 5];
+let numForInvert2 = [1,-2,4,-5];
 
 function invert(arr) {
-    return arr.map((item) => {
-        if (item >= 0 || item <= 0) {
-            return item *= -1;
-        }
-        }
-    )
+    return arr.map((item) => item *= -1);
 };
 console.log(invert(numForInvert0));// --> [ 1, 2, 4, 5 ]
 console.log(invert(numForInvert1));// --> [ -1, -2, -4, -5 ]
-console.log(invert(numForInvert2));// --> [ -1, 2, -4, 5 ]
+console.log(invert(numForInvert2));// --> [ -1, 2, -4, 5 ] 
