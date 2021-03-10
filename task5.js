3eef401a8f4fef7d188bb3556fa0a12fa0195c49
Eric Belecov <ericbelecov@gmail.com>
2021-03-11 02:47:31 +0300

    Third commit

--- task5.js
+++ task5.js
@@ -1,19 +1,27 @@
-// Задача №5
-// Банкоматы допускают 4 или 6-значные ПИН-коды, 
-// а ПИН-коды не могут содержать ничего, кроме 4 цифр или 6 цифр.
-// Если функции передана правильная строка PIN, 
-// верните true, иначе верните false.
+/*
+Задача №5
+Банкоматы допускают 4 или 6-значные ПИН-коды, 
+а ПИН-коды не могут содержать ничего, кроме 4 цифр или 6 цифр.
+Если функции передана правильная строка PIN, 
+верните true, иначе верните false.*/
 
 function pinCode(str) {
-    let ArrOfStr = str.split('');  // преобразование строки в массив строк
-    let ArrOfNum = ArrOfStr.map(Number); // преобразование массива строк в массив чисел
-    if (ArrOfNum.length == 4) {
-        return true;
-    } else if (ArrOfNum.length == 6) {
-        return true;
+    let ArrOfNum = str.split('').map(Number);
+
+    function checkLength(arr) {
+        if (ArrOfNum.length == 4) {
+            return true;
+        } else if (ArrOfNum.length == 6) {
+            return true;
+        }
+        else {
+            return false
+        }
     }
-    else {
-        return false
+    if (ArrOfNum.every(item => !isNaN(item))) {
+        return checkLength();
+    } else {
+        return false;
     }
 };
 
