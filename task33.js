96b0e3f23342f0305b8c6595f685c4274aee84b0
Eric Belecov <ericbelecov@gmail.com>
2021-03-20 22:15:41 +0300

    Second commit

--- task33.js
+++ task33.js
@@ -5,11 +5,8 @@
 // `comparison('string', 'StRiNg')`
 
 
-function comparison(str1, str2) {
-    let s1 = str1.toLowerCase(); // преобразовываем первый аргумент str1 в нижний решгистр, на случай если в нем будут буквы верхнего рег. 
-    if (s1 === str2.toLowerCase()) {  // условие "если все буквы str1 нижнего регистра будут равны str1 нижнего регистра
-        return true    // то вернуть true
-    }
-};
+let comparison = (str1, str2) => str1.toLowerCase() === str2.toLowerCase() ? true : false;   
+    
 console.log(comparison('string', 'StRiNg')); // true
 console.log(comparison('Javascript', 'JaVaScRiPt')); // true
+console.log(comparison('belarus', 'BELARUS')); // true
